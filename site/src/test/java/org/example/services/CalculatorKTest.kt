df50c67cb297fd9914ccec57e0f71e4ca7818944
LinXitoW <mriegler@gmx.de>
2018-06-24 16:20:21 +0200

    add another kotlin test example

--- site/src/test/java/org/example/services/CalculatorKTest.kt
+++ site/src/test/java/org/example/services/CalculatorKTest.kt
@@ -1,7 +1,9 @@
 package org.example.services
 
+import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.BehaviorSpec
+import io.kotlintest.specs.FreeSpec
 
 class CalculatorKTest: BehaviorSpec({
     Given("a new Calculator") {
@@ -19,6 +21,34 @@ class CalculatorKTest: BehaviorSpec({
                     calculator.lastResult
                 }
             }
+        }
+
+    }
+})
+
+class CalculatorK2Test: FreeSpec({
+    "given a new calculator" - {
+        val calculator = Calculator()
+
+        "when i calculate, it throws an exception" {
+            shouldThrow<IllegalStateException> {
+                calculator.calculate("+")
+            }
+        }
+
+        "when i get the last result, it throws" {
+            shouldThrow<IllegalStateException> {
+                calculator.lastResult
+            }
+        }
+    }
+
+    "given a calculator with an entered number, when i add a numder and calculate, i get the result" {
+        val calculator = Calculator().apply {
+            enterNumber(2)
+            enterNumber(2)
+            calculate("+")
+            lastResult shouldBe 4
         }
 
     }
